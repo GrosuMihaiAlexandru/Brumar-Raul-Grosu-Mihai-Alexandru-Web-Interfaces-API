f904cb644ccf485bdb8b3df7b35427dedf8bbf82
Raul Brumar <raulbrumar@yahoo.de>
2020-02-22 21:49:22 +0200

    added mongoose to the project

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const oauth = require('./routes/oauth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
 const app = express();
 const port = 3000;
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+    console.log("connected to mongodb");
+});
 
 // oauth router
 app.use('/users/auth', oauth);
