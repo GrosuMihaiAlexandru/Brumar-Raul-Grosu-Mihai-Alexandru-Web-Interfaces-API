d8d828e7294191af41c516c62ea3a02d6a11e2eb
Raul Brumar <raulbrumar@yahoo.de>
2020-02-23 18:02:32 +0200

    Adding user to database after login done (creates duplicate users)

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
-const keys = require('./keys')
+const keys = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
     new GoogleStrategy({
@@ -12,4 +13,11 @@ passport.use(
     // callback function
     console.log('passport callback function fired')
     console.log(profile);
-}))
+
+    new User({
+        username: profile.displayName,
+        googleId: profile.id
+    }).save().then((newUser) => {
+        console.log('New user has been created: ' + newUser);
+    }); // the new user is saved asyncronously
+}));
