6f5f48f1e29cca1b8d8092e087961ea67bbba3da
Alexandru Grosu <alex.gm98@yahoo.com>
2020-02-24 18:17:36 +0200

    Added put and delete endpoints for /items

--- routes/items-routes.js
+++ routes/items-routes.js
@@ -17,6 +17,7 @@ const authCheck = (req, res, next) => {
 }
 
 router.post('/', authCheck, (req, res) => {
+    // Checking if the json is valid
     if (req.body.hasOwnProperty('title') && req.body.hasOwnProperty('description') && req.body.hasOwnProperty('category') && req.body.hasOwnProperty('location') &&
     req.body.hasOwnProperty('images') && req.body.hasOwnProperty('askingPrice') && req.body.hasOwnProperty('dateOfPosting') && req.body.hasOwnProperty('deliveryType') &&
     req.body.hasOwnProperty('sellerInfo'))
@@ -34,13 +35,98 @@ router.post('/', authCheck, (req, res) => {
         userId: req.user.id
     }).save().then((newItem) => {
         console.log('New item has been created ' + newItem);
-        res.status(201).send('Item Created')
+        res.status(201).send('Item Created');
       })
     }
     else
     {
+        console.log(req.body);
         res.sendStatus(400);
     }
 });
+
+router.put('/', authCheck, (req, res) => {
+    if (req.body.hasOwnProperty('id'))
+    {
+        Item.findById(req.body.id).then((currentItem) => {
+            if (currentItem)
+            {
+                // Every other property besides id is not mandatory
+                // Only the given properties will be updated
+                if (req.body.hasOwnProperty('title'))
+                {
+                    currentItem.title = req.body.title;
+                }
+                if (req.body.hasOwnProperty('description'))
+                {
+                    currentItem.description = req.body.description;
+                }
+                if (req.body.hasOwnProperty('category'))
+                {
+                    currentItem.category = req.body.category;
+                }
+                if (req.body.hasOwnProperty('location'))
+                {
+                    currentItem.location = req.body.location;
+                }
+                if (req.body.hasOwnProperty('images'))
+                {
+                    currentItem.images = req.body.images;
+                }
+                if (req.body.hasOwnProperty('askingPrice'))
+                {
+                    currentItem.askingPrice = req.body.askingPrice;
+                }
+                if (req.body.hasOwnProperty('dateOfPosting'))
+                {
+                    currentItem.dateOfPosting = req.body.dateOfPosting;
+                }
+                if (req.body.hasOwnProperty('deliveryType'))
+                {
+                    currentItem.deliveryType = req.body.deliveryType;
+                }
+                if (req.body.hasOwnProperty('sellerInfo'))
+                {
+                    currentItem.sellerInfo = req.body.sellerInfo;
+                }
+
+                currentItem.save().then(() => {
+                    console.log('Item Updated to ' + currentItem);
+                    res.status(200).send('Item Updated');
+                })
+            }  
+            else
+            {
+                res.status(404).send("Item not found");
+            } 
+        })
+    }
+    else
+    {
+        console.log(req.body);
+        res.sendStatus(400);
+    }
+})
+
+router.delete('/', authCheck, (req, res) => {
+    if (req.body.hasOwnProperty('id'))
+    {
+        Item.findOneAndRemove({_id: req.body.id}, req.body, function(err,data)
+        {
+            if(!err){
+                res.status(200).send("Deleted");
+            }
+            else
+            {
+                res.sendStatus(404);
+            }
+        });
+        
+    }
+    else
+    {
+        res.sendStatus(400); 
+    }
+})
 
 module.exports = router;
