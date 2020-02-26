ba421da76d9ebbf59d9cb29ae4f9217a9cc215e6
Alexandru Grosu <alex.gm98@yahoo.com>
2020-02-26 18:03:58 +0200

    Added basic and jwt strategies

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,15 @@
 const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
 const GoogleStrategy = require('passport-google-oauth20');
+const jwt = require('jsonwebtoken');
+const JwtStrategy = require('passport-jwt').Strategy,
+      ExtractJwt = require('passport-jwt').ExtractJwt;
 const keys = require('./keys');
 const User = require('../models/user-model');
+const bcrypt = require('bcryptjs');
 
-passport.serializeUser((user, done) => {
-    done(null, user.id);
+passport.serializeUser((obj, done) => {
+    done(null, obj.user.id);
 });
 
 passport.deserializeUser((id, done) => {
@@ -12,6 +17,44 @@ passport.deserializeUser((id, done) => {
         done(null, user);
     });
 });
+
+// Basic auth
+passport.use(new BasicStrategy((username, password, done) => {
+    User.findOne({googleId: profile.id}).then((currentUser) => {
+        if (currentUser)
+        {
+            // Username not found
+            console.log("HTTP Basic username not found");
+            return done(null, false, { message: "HTTP Basic username not found" });
+        }
+
+        if(bcrypt.compareSync(password, user.password) == false) {
+            // Password does not match
+            console.log("HTTP Basic password not matching username");
+            return done(null, false, { message: "HTTP Basic password not found" });
+          }
+          return done(null, user);
+    })
+}))
+
+// JWT auth
+passport.use(
+    new JwtStrategy({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: keys.secret
+    }, (jwt_payload, done) => {
+        console.log("Processing JWT payload for token content:");
+         console.log(jwt_payload);
+
+         const now = Date.now() / 1000;
+        if(jwt_payload.exp > now) {
+            done(null, jwt_payload.user);
+        }
+        else {// expired
+            done(null, false);
+        }
+    }) 
+)
 
 passport.use(
     new GoogleStrategy({
@@ -21,12 +64,14 @@ passport.use(
         clientSecret: keys.google.clientSecret
 }, (accessToken, refreshToken, profile, done) => {
     // check if user already exists
+    //console.log("passport callback")
     User.findOne({googleId: profile.id}).then((currentUser) => {
         if (currentUser)
         {
             // user already exists
-            console.log('User is already registered: ' + currentUser);
-            done(null, currentUser);
+            console.log('User is already registered: ' + accessToken);
+            done(null, { user: currentUser, accessToken: accessToken});
+            
         }
         else
         {
